fa98ee899d3e1286fafbe2d3bdd766dad43ce31f
BT_1N3_03 <n>
2018-12-11 09:26:11 -0500

    ez

--- src/Deck.java
+++ src/Deck.java
@@ -1,14 +1,13 @@
 import java.util.ArrayList;
 
 public class Deck {
-    private ArrayList<Card> unDealt;
-    private ArrayList<Card> Dealt;
+    private ArrayList<Card> unDealt = new ArrayList<>();
+    private ArrayList<Card> Dealt = new ArrayList<>();
 
     public Deck(String[] suit, String[] rank, int[] pointValue){
         for(int i=0;i<suit.length;i++){
             for(int k=0;k<rank.length;k++) {
-                Card temp = new Card(rank[i], suit[k], pointValue[k]);
-                System.out.println(temp);
+                Card temp = new Card(rank[k], suit[i], pointValue[k]);
                 unDealt.add(temp);
             }
         }
@@ -31,14 +30,14 @@ public class Deck {
         return Dealt.get(Dealt.size()-1);
     }
 
-    public void Shuffle(){
-        for(int i=0;i<Dealt.size();i++){
-            unDealt.add(Dealt.get(i));
-            Dealt.remove(i);
+    public void shuffle(){
+        while(Dealt.size()>0){
+            unDealt.add(Dealt.get(0));
+            Dealt.remove(0);
         }
 
-        for(int k=51;k>0;k++){
-            int r = (int)Math.random()*51;
+        for(int k=51;k>0;k--){
+            int r = (int)(Math.random()*51);
             Card temp = unDealt.get(k);
             unDealt.set(k,unDealt.get(r));
             unDealt.set(r,temp);
--- src/DeckTester.java
+++ src/DeckTester.java
@@ -1,14 +1,19 @@
 public class DeckTester {
-    public static void main(String[] args){
-        String[] suit = {"Hearts", "Diamonds", "Spades", "Ace"};
-        String[] rank = {"1", "2", "3", "4"};
-        int[] pointValue = {1,2,3,4};
+    public static void main(String[] args) {
+        String[] suit = {"Hearts", "Diamonds", "Spades", "Clubs"};
+        String[] rank = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+        int[] pointValue = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 
-        Deck testDeck = new Deck(suit,rank,pointValue);
+        Deck testDeck = new Deck(suit, rank, pointValue);
 
-        for(int i=0;i<testDeck.size();i++){
+        for (int i = 0; i < 52; i++) {
             System.out.println(testDeck.deal());
         }
 
+        testDeck.shuffle();
+        System.out.println("\nShuffled\n");
+        for (int i = 0; i < 52; i++) {
+            System.out.println(testDeck.deal());
+        }
     }
 }
